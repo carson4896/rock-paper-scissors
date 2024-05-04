3fdcf5668a8ad4190a89745571572ca988f56cc4
Carson <32146101+carson4896@users.noreply.github.com>
2024-05-04 10:49:50 -0400

    Updated the Javascript to allow for a more streamlined logic to evaluate the human and computer choice rather than have 1 rule per scenario.

--- RPS.js
+++ RPS.js
@@ -1,80 +1,80 @@
-//Write a function that randomly returns rock, paper, or scissors from the computer
-    //FUNCTION getComputerChoice
-    //Get a random value between 0 and 100
-    //If value is between 0 and 33, return "Rock"
-    //If value is between 33 and 66, return "Paper"
-    //If value is between 66 and 100, return "Scissors"
+// getComputerChoice() will randomly return rock, paper, or scissors for the computer
+    // rand will generate a random number between 0 and 2
+    // choices will be an array of rock, paper, and scissors
+    // return a selction from the choices array, using the randomly generated number
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random()*100);
-    if (randomNumber >= 0 && randomNumber < 33) {
-        return "rock"
-    } else if (randomNumber >=33 && randomNumber <67) {
-        return "paper"
-    } else if (randomNumber >=67 && randomNumber <= 100) {
-        return "scissors"
-    }
+    const rand = Math.floor(Math.random() * 3);
+    const choices = ["rock", "paper", "scissors"];
+    return choices[rand];
 }
 
-//Write a function to get the human choice//Convert the human selection to be lowercase so that the way it was entered by the user does not matter
-    //FUNCTION getHumanChoice
-    //Provide an input prompt for the user to enter rock, paper, or scissors
-    //Convert the human selection to be lowercase so that the way it was entered by the user does not matter
+// getHumanChoice() will prompt the user to enter a value, and will convert it to lowercase
+    // If the response is not valid, ask the user to re-enter
 function getHumanChoice() {
-    let humanChoice = prompt("Please enter your choice of rock, paper, or scissors: ");
-        return humanChoice.toLowerCase();
-}
-
-//Declare Player Score variables to keep track of player score
-    //Create a global variable for the human score and initialize it to zero
-    //Create a global variable for the computer score and initalize it to zero
-let humanScore = 0
-let computerScore = 0
-
-//Write a function to evaluate the human and computer choices to determine the winner
-    //FUNCTION playRound with two parameters (humanChoice, and computerChoice)
-    //Create an if statement that first evaluates if there is a tie
-    //Else if computer rock beats scissors, respond with "You lose! Rock beats Scissors"
-    //Else if computer paper beats rock, "You lose! Paper beats Rock"
-    //Else if computer scissors beat paper "You lose! Scissors beat Paper"
-    //Else if user rock beats scissors, respond with "You Win! Rock beats Scissors"
-    //Else if user paper beats rock, "You Win! Paper beats Rock"
-    //Else if user scissors beat paper "You Win! Scissors beat Paper"
-    //Increment the winner's score by 1 with the round
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        return "It's a tie! Go again!"
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        computerScore = computerScore + 1
-        return "You lose! Rock beats Scissors"
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        computerScore = computerScore + 1
-        return "You lose! Paper beats Rock"
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        computerScore = computerScore + 1
-        return "You lose! Scissors beats Paper"
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        humanScore = humanScore + 1
-        return "You win! Paper beats Rock" 
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        humanScore = humanScore + 1
-        return "You win! Rock beats Scissors"
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        humanScore = humanScore + 1
-        return "You win! Scissors beat Paper"
+    const choices = ["rock", "paper", "scissors"];
+    while (true) {
+        const choice = prompt("Please enter your choice of rock, paper, or scissors: ");
+        const choiceLower = choice.toLowerCase();
+        for (c of choices) {
+            if (c === choiceLower) {
+                return c;
+            }
+        }
+        alert("Invalid Input. Try again");
     }
 }
 
-// Declare variables that will store each choice
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
+// playGame() contains the game logic, in this function you will have:
+    // Two variables declared to track the human and computer score (starting at 0)
+    // playRound() is a nested function that will hold the actual game evaluation logic (go to line 34 to read details)
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0    
 
-// Display the values of each selection in the console
-console.log(humanSelection);
-console.log(computerSelection);
+    // playRound() is the nested function that contains the actual logic behind the winner/loser based on their respective choices
+        // There are two parameters of humanChoice and computerChoice
+        // If the human and computer choose the same, it's a tie
+        // If the human chooses a winning option, alert the user that they won and add a point to the human score
+        // Otherwise, the human chose a losing option, alert the user that they lost and add a point to the computer score
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            alert(`You both chose ${humanChoice}, so it's a tie!`)
+        } else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")
+        ) {
+            alert(`You chose ${humanChoice}, Computer chose ${computerChoice}. You win!`);
+            humanScore++;
+        } else {
+            alert(`You chose ${humanChoice}, Computer chose ${computerChoice}. You lose :(`);
+            computerScore++;
+        }
+    }
 
-// Display the outcome of the round
-console.log(playRound(humanSelection, computerSelection))
+    // This for loop will run for 5 rounds, each round will:
+        // call the getHumanChoice function to get the input of the human
+        // call the getComputerChoice function to get the input of the computer
+        // call the playRound function to store the human and computer selections
+        // alert the user of the score
+        // increase the count of the round before moving to the next round
+    for (let round = 1; round <=5; round++) {
+        alert(`Round ${round}, Get Ready!`);
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
 
-//Display the new score after the round has concluded
-console.log(humanScore)
-console.log(computerScore)
+        playRound(humanSelection, computerSelection);
+
+        alert(`Your Score: ${humanScore}. Computer Score: ${computerScore}`)
+    }
+
+    if (humanScore > computerScore) {
+        alert("You won the game!")
+    } else if (computerScore > humanScore) {
+        alert("You lose the game, sorry :(")
+    } else {
+        alert("The game was a draw!")
+    }
+}
+
+let play = playGame();
